7e71d7bbd634fd5de540ded7e93427d646701238
meri <mariana_asavoaiei@outlook.com>
2023-09-27 17:39:56 +0100

    added an Aside and Notification&Profile components

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My App</h1>
+      {/* Add other header content */}
+    </header>
+  );
+};
+
+export default Header;
