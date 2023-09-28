908e32176387afeded3ef047d212c957b94cf15d
meri <mariana_asavoaiei@outlook.com>
2023-09-28 17:34:52 +0100

    added styles to the Aside component

--- src/Aside.js
+++ src/Aside.js
@@ -1,25 +1,24 @@
 // Aside.js
-import React from 'react';
+import React from "react";
 
 const Aside = () => {
   return (
-    <aside style={{width:'256px', height:'1024px',  background: '#5F799E'}}>
-      <div className="files-icon"> {/* Add CSS styling for the icon */}
-        {/* Add your files icon here */}
+    <aside classnName="aside">
+      <div className="files-icon">
+        <img src="dashIcon.png" alt="Dashboard Icon" />
+        <img src="dashIcon.png" alt="New admission" />
+        <img src="dashIcon.png" alt="Individual" />
+        <img src="dashIcon.png" alt="Corporate" />
+        <img src="dashIcon.png" alt="Application" />
+        <img src="dashIcon.png" alt="Status roles" />
       </div>
-      <ul className="title-list"> {/* Add CSS styling for the list */}
-       <li>Dashboard</li>
-        <img src="dashIcon.png" alt="Dashboard Icon" /> 
-       <li>New admission</li>
-        <img src="dashIcon.png" alt="New admission"/>
-       <li>Individual</li>
-        <img src="dashIcon.png" alt="Individual"/> 
-       <li>Corporate</li>
-        <img src="dashIcon.png" alt="Corporate"/> 
-       <li>Application</li>
-        <img src="dashIcon.png" alt="Application"/> 
-       <li>Status roles</li>
-        <img src="dashIcon.png" alt="Status roles"/> 
+      <ul className="title-list">
+        <li>Dashboard</li>
+        <li>New admission</li>
+        <li>Individual</li>
+        <li>Corporate</li>
+        <li>Application</li>
+        <li>Status roles</li>
       </ul>
     </aside>
   );
