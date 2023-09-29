02d063caf148576fb6122825885a8437272f6f6c
meri <mariana_asavoaiei@outlook.com>
2023-09-29 12:37:48 +0100

    added the FormOptions component to the PersonalInfo page

--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Nav from "./Nav";
 import Header from "./Header";
-import NotificationAndProfile from "./NotificationAndProfile";
+import NotificationAndProfile from  "./NotificationAndProfile";
 import Aside from "./Aside";
 import SaveButton from "./SaveButton";
 import { Link } from "react-router-dom";
 import "./App.css";
 import "./styles.css";
+import FormOptions from "./FormOptions.js";
+
 
 const PersonalInfo = () => {
   /* Array of items for Nav li, so that we can map through them and list them as they appear in each nav, on page*/
@@ -25,26 +27,9 @@ const PersonalInfo = () => {
       <Aside />
       <div className="formDivPersonal">
         <h1>Medical declaration</h1>
-        <form className="formPersonal">
-          <div className="questionSet">
-            <p>Does the pacient drink alcohol?</p>
-            <label>
-              <input type="radio" name="answer" value="yes" /> Yes
-            </label>
-            <label>
-              <input type="radio" name="answer" value="no" /> No
-            </label>
-            <p>How many times a week?</p>
-            <label>
-              Select an option:
-              <select>
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </select>
-            </label>
-          </div>
-        </form>
+        <div className="formDivPersonal">
+          <FormOptions className="formPersonal" />
+        </div>
         <Link to="/company">
           <SaveButton />
         </Link>
