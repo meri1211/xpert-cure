7823be4f6ec3a452c9a7093ddd8dcaccd70504aa
meri <mariana_asavoaiei@outlook.com>
2023-09-29 13:25:30 +0100

    added a Router component to the routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 import "./App.css";
 import React from "react";
 /*import { Router, Route, Switch } from "react-router-dom";*/
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PersonalInfo from "./PersonalInfo"; // Import the PersonalInfo component
 import CompanyDetails from "./CompanyDetails"; // Import the CompanyDetails component
-import './styles.css'
-
+import "./styles.css";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<PersonalInfo />} />
-      <Route path="/company" element={<CompanyDetails />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PersonalInfo />} />
+        <Route path="/company" element={<CompanyDetails />} />
+      </Routes>
+    </Router>
   );
 };
 
